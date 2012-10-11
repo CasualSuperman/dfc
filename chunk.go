4e4e8f56d800f7ae2540046dc1ceef47002c3e39
Robert Wertman <robert.wertman@gmail.com>
2012-10-10 21:45:55 -0400

    Started simplex noise terrain generation.

--- chunk.go
+++ chunk.go
@@ -1,23 +1,7 @@
 package main
 
-const chunk_horiz_dim = 32
-const chunk_vert_dim = 128
-
-/*
-type Chunk struct {
-	tiles [chunk_vert_dim][chunk_horiz_dim][chunk_horiz_dim]Tile
-}
-*/
-
 type octree struct {
 	ranges   [3]int
 	subtrees [2][2][2]Chunk
 	base     Tile
-}
-
-type Chunk interface {
-	At(x, y, z int) Tile
-	From(x, y, z int) interface {
-		To(x, y, z int) Chunk
-	}
 }
---
+++ terrain/terrain.go
@@ -0,0 +1,18 @@
+package terrain
+
+import "github.com/larspensjo/Go-simplex-noise/simplexnoise"
+
+const chunk_horiz_dim = 32
+const chunk_vert_dim = 128
+
+type Chunk interface {
+	Interacter
+	At(x, y, z int) Tile
+	From(x, y, z int) interface {
+		To(x, y, z int) Interacter
+	}
+}
+
+type Interacter interface {
+	
+}
