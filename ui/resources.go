163ae06e7141d713a04b63fb588c784c34461a99
Robert Wertman <robert.wertman@gmail.com>
2012-11-03 17:30:13 -0400

    Pros/Cons of libs

--- ui/resources.go
+++ ui/resources.go
@@ -1,3 +1,13 @@
 package ui
 
+// ============ Termbox =============
 // https://github.com/nsf/termbox-go/
+// Pros:
+//	Supports unicode
+//	Cross-platform
+// Cons:
+//	In beta
+//	Some problems with weird key combos
+//	Not 256 Colors, only 8
+// Notes:
+//	Event-driven
