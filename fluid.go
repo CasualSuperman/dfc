4e25bd99082b446731bb0a41112baa0ce3be1616
Robert Wertman <robert.wertman@gmail.com>
2012-12-11 14:31:24 -0500

    Changed location of dirty bit in fluids.
    Reflects the maximum depth of water (or other fluids) being seven, is
    now also logically the highest depth possible. Decided to use the fourth
    bit as the dirty flag instead of the 8th.

--- fluid.go
+++ fluid.go
@@ -3,13 +3,13 @@ package main
 type Fluid uint8
 
 func (f Fluid) Clean() bool {
-	return (uint8(f) & 0x80) == 0x80
+	return (uint8(f) & 0x08) == 0x08
 }
 
 func (f Fluid) Type() uint8 {
-	return (uint8(f) & 0x70) >> 4
+	return (uint8(f) & 0xF0) >> 4
 }
 
 func (f Fluid) Depth() uint8 {
-	return (uint8(f) & 0x0F)
+	return (uint8(f) & 0x07)
 }
